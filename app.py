2a6295106b57cc1bc4a54ffc2d125b3088f26871
Jacob Zufall <jacobzufall@gmail.com>
2022-07-08 10:06:22 -0400

    Update app.py
    Added game stats

--- app.py
+++ app.py
@@ -12,10 +12,15 @@ playerMove = "rock"
 playerWin = "lose"
 playerResponse = "yes"
 
+gameWin = 0
+gameLose = 0
+gameDraw = 0
+
 while gameRunning:
     gameMove = random.choice(moveTable)
     print("What is your move?")
     playerMove = input()
+
     while playerMove.casefold() not in moveTable:
         print("Sorry, that's not a valid move. Try again.")
         playerMove = input()
@@ -26,7 +31,6 @@ while gameRunning:
         while playerResponse.casefold() not in choiceTable:
             print("Invalid response.")
             playerResponse = input()
-
     elif playerMove.casefold() != gameMove:
 
         # Runs if the player plays rock.
@@ -51,27 +55,39 @@ while gameRunning:
                 playerWin = "Win"
 
     if playerWin == "Win":
+        gameWin += 1
         print("You played: " + playerMove.casefold())
         time.sleep(1)
         print("Game played: " + gameMove)
         time.sleep(1)
         print("You won. Congratulations!")
+        print("W: " + str(gameWin))
+        print("L: " + str(gameLose))
+        print("D: " + str(gameDraw))
         print("Play again?")
 
     elif playerWin == "Lose":
+        gameLose += 1
         print("You played: " + playerMove.casefold())
         time.sleep(1)
         print("Game played: " + gameMove)
         time.sleep(1)
         print("You lost. Better luck next time!")
+        print("W: " + str(gameWin))
+        print("L: " + str(gameLose))
+        print("D: " + str(gameDraw))
         print("Play again?")
 
     elif playerWin == "Draw":
+        gameDraw += 1
         print("You played: " + playerMove.casefold())
         time.sleep(1)
         print("Game played: " + gameMove)
         time.sleep(1)
         print("It's a draw!")
+        print("W: " + str(gameWin))
+        print("L: " + str(gameLose))
+        print("D: " + str(gameDraw))
         print("Play again?")
 
     # This determines if the code should run again, or if the while loop should end.
